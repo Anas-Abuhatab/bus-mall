8627ac2474a762409e905713777acd14d73bc281
rymbaniali <rym.ayasrah@hotmail.com>
2021-08-10 23:15:02 +0300

    all done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 'use strict';
-
 let arrOfImg = [ //! STEP NUMBER ONE  get the constractor property source
   './img/bag.jpg',
   './img/banana.jpg',
@@ -21,6 +20,9 @@ let arrOfImg = [ //! STEP NUMBER ONE  get the constractor property source
   './img/water-can.jpg',
   './img/wine-glass.jpg',
 ];
+
+let prevArr = [];
+
 let firstImg = document.getElementById( 'firstImg' ); //!STEP #5 getElementById
 let secondImg = document.getElementById( 'secondImg' );
 let thirdImg = document.getElementById( 'thirdImg' );
@@ -55,18 +57,28 @@ let theRandomthree = 0;
 
 
 
+
 function render(){ //! STEP NUMBER FOUR make render go to JS section on html and see the id
 
-  theRandomone = random( 0,arrOfImg.length - 1 );
-  theRandomtwo = random( 0,arrOfImg.length - 1 );
-  theRandomthree = random( 0,arrOfImg.length - 1 );
-
-  while ( theRandomone === theRandomtwo || theRandomtwo === theRandomthree || theRandomthree === theRandomone ){
-    theRandomone = random( 0,arrOfImg.length - 1 );
+  do {theRandomone = random( 0,arrOfImg.length - 1 );
     theRandomtwo = random( 0,arrOfImg.length - 1 );
     theRandomthree = random( 0,arrOfImg.length - 1 );
   }
 
+  while ( theRandomone === theRandomtwo ||
+     theRandomtwo === theRandomthree ||
+      theRandomthree === theRandomone ||
+      prevArr.includes( theRandomone ) ||
+      prevArr.includes( theRandomtwo ) ||
+      prevArr.includes( theRandomthree )
+  );
+
+
+  console.log( prevArr );
+
+  prevArr = [theRandomone ,theRandomtwo,theRandomthree];
+
+  console.log( prevArr );
   if ( theRandomone !== theRandomtwo && theRandomone !== theRandomthree && theRandomthree !== theRandomtwo ){
 
     firstImg.src = Mall.mallAll[theRandomone].imgSrc; //!STEP #6 add parameter to id
@@ -76,17 +88,27 @@ function render(){ //! STEP NUMBER FOUR make render go to JS section on html and
     Mall.mallAll[theRandomone ].numofshow++;
     Mall.mallAll[ theRandomtwo ].numofshow++;
     Mall.mallAll[ theRandomthree ].numofshow++;
-
   }
 
-
-
 }
-
 render();
 
 function random( min, max ) {
-  return Math.floor( Math.random() * ( max - min + 1 ) + min );
+  let random;
+  let allowed;
+
+  do{
+    random = Math.floor( Math.random() * ( max - min + 1 ) + min );
+    allowed = true;
+    for ( let i = 0; i < prevArr.length; i++ ) {
+      if( random === prevArr[i] ){
+        allowed = false;
+      }
+
+    }
+  }while( !allowed );
+
+  return random;
 }
 
 
@@ -110,10 +132,8 @@ function clicker( event ){
     cont++;
     render();
 
-  }
-  if( cont >= maxcont ){
-    image.removeEventListener( 'click' , clicker );
-  }
+  }else{createChart();}
+
 
 }
 
@@ -125,9 +145,75 @@ function result( ){
   for ( let i = 0; i < Mall.mallAll.length; i++ ) {
 
     let li = document.createElement( 'li' );
-    li.textContent = `${Mall.mallAll[i].imgName} had ${Mall.mallAll[i].numofshow} votes ,and was seen ${Mall.mallAll[i].clickCount}
+    li.textContent = `${Mall.mallAll[i].imgName} had ${Mall.mallAll[i].clickCount} votes ,and was seen ${Mall.mallAll[i].numofshow}
   times`;
     ul.appendChild( li );
 
   }
 }
+
+function createChart (){
+
+  let nameArr = [];
+  let showArr = [];
+  let click = [];
+  for ( let i = 0; i < Mall.mallAll.length; i++ ) {
+    nameArr.push( Mall.mallAll[i].imgName );
+    showArr.push( Mall.mallAll[i].numofshow );
+    click.push( Mall.mallAll[i].clickCount );
+
+  }
+
+
+  let ctx = document.getElementById( 'myChart' ).getContext( '2d' );
+  let myChart = new Chart( ctx, {
+    type: 'bar',
+    data: {
+      labels:nameArr ,
+      datasets: [{
+        label: '# of Shows/Image',
+        data: showArr,
+
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 20
+      },
+      {
+        label: '# of clicks',
+        data: click,
+
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 5
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  } );
+}
+
